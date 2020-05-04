42b080858a91627e2cc5d7911ab2d6b2fd5b70fb
Lauren Miggins <lauren.miggins@gmail.com>
2020-05-04 11:29:42 -0400

    adding smoke particles

--- ufo.js
+++ ufo.js
@@ -4,6 +4,8 @@ var inc;
 var slider;
 var x;
 var y = 300;
+let particles = [];
+
 
 function setup() {
     createCanvas(windowWidth, (windowHeight - 200));
@@ -13,6 +15,9 @@ function setup() {
     //position and increment set up
     pos = 0.0;
     inc = 3.0;
+
+    //particles setup
+    
 }
 function draw () {
     clear();
@@ -27,8 +32,19 @@ function draw () {
 
     //earth
     fill('green');
-    rect(0, windowHeight - y, windowWidth, y)
+    rect(0, 520, windowWidth, 200)
     
+    //ufo trails
+    let p = new Particle();
+    particles.push(p);
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].show();
+        if (particles[i].finished()){
+            particles.splice(i, 1);
+        }
+    }
+
     // moving UFO
     fill('orange')
     noStroke();
@@ -38,5 +54,31 @@ function draw () {
     //tractor beam
     fill('rgba(255,255,0, 0.33)');
     triangle(x, y + 20, windowHeight, windowHeight + y, windowWidth + y, y + x);
+
     
+   
+}
+    //creating new particles
+class Particle {
+    constructor() {
+        this.x = x-50;
+        this.y = y+20;
+        this.vx = random(-5, -1);
+        this.vy = random(-1, 0);
+        this.alpha = 255;
+    }
+    finished() {
+        return this.alpha < 0;
+    }
+    update(){
+        this.x += this.vx;
+        this.y += this.vy
+        this.alpha -= 5;
+    }
+    show() {
+        noStroke;
+        fill(255, this.alpha);
+        ellipse(this.x, this.y, 16);
+    }
+
 }
